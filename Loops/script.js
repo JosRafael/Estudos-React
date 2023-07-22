a0a7c57e51375a43185e71ffd72fee308d4908bd
José Rafael <rafaelmatiashue@gmail.com>
2023-07-22 10:57:09 -0300

    Loops

---
+++ Loops/script.js
@@ -0,0 +1,16 @@
+// While = Enquanto
+x = 0;
+
+while(x<10){
+    document.write("</br> O valor do X : " + x);
+    x++;    
+}
+
+x=4;
+
+document.write("</br></br></br> O valor do X esta valendo : " + x+ "</br></br>");
+// For= Para
+
+for(i=0; i<x; i++){
+    document.write("</br> O valor do I : " + i);
+}
