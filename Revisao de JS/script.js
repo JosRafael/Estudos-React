09f0774c9a7b91942f56293e834724ffc043f016
José Rafael <rafaelmatiashue@gmail.com>
2023-07-22 10:17:25 -0300

    functions ok

--- Revisao de JS/script.js
+++ Revisao de JS/script.js
@@ -1,6 +1,30 @@
+//funçao entrar
 function entrar(){
     var area=document.getElementById("area");
     var texto = prompt("Qual o seu nome?");
 
-    area.innerHTML = texto;
+
+    if(texto == '' || texto==null){
+        alert("Digite seu nome novamente");
+        area.innerHTML = "Bem vindo ...";
+    }else{
+        area.innerHTML = "Bem vindo " + texto;
+    }   
+}
+function entrar2(nome){
+    var area=document.getElementById("area2");
+    var texto = prompt("Qual o seu sobrenome?");
+
+    if(texto == '' || texto==null){
+        alert("Digite seu nome novamente");
+        area.innerHTML = "Bem vindo ...";
+    }else{
+        area.innerHTML = nome + " " + texto;
+    }  
+}
+function entrar3(nome, texto){
+    var area=document.getElementById("area3");
+    var idade = prompt("Qual a sua idade?");
+
+    area.innerHTML = nome + " " + idade;
 }
